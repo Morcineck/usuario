2857e082dc94eded35f368da017d6ccaf86167d8
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-05-14 22:49:03 -0300

    adicionadas classes de security

---
+++ src/main/java/com/morcineck/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.morcineck.usuario.infrastructure.repository;
+
+import com.morcinek.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
