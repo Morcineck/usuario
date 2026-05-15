b92d4bcd5f8b462c8b0530770a2d7d4b9af0fdf2
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-05-15 00:58:10 -0300

    adicionado método que salva dados usuário

---
+++ src/main/java/com/morcineck/usuario/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.morcineck.usuario.controller;
+
+import com.morcineck.usuario.business.UsuarioService;
+import com.morcineck.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("*/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+}
