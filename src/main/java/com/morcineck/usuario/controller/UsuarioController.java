bb318c087b4dd4a7da27b639ca538254f5c74888
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-05-20 02:00:16 -0300

    atualização/ correção endpoints de login, busca e delete de usuario

--- src/main/java/com/morcineck/usuario/controller/UsuarioController.java
+++ src/main/java/com/morcineck/usuario/controller/UsuarioController.java
@@ -3,8 +3,13 @@ package com.morcineck.usuario.controller;
 
 import com.morcineck.usuario.business.UsuarioService;
 import com.morcineck.usuario.business.dto.UsuarioDTO;
+import com.morcineck.usuario.infrastructure.entity.Usuario;
+import com.morcineck.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +18,30 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTo){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTo.getEmail(),
+                        usuarioDTo.getSenha())
+
+        );
+        return "Bearer" + jwtUtil.generateToken(authentication.getName());
+    }
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsiarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email){
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
     }
 }
