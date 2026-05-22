f5917f0e0385679d1961a77c469becef0a4820e8
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-05-22 00:14:29 -0300

    Adicionados metodos de gravar endereço e telefone do usuário

--- src/main/java/com/morcineck/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/morcineck/usuario/business/converter/UsuarioConverter.java
@@ -130,4 +130,24 @@ public class UsuarioConverter {
                 .build();
     }
 
+    public Endereco paraEnderecoEntity(EnderecoDTO dto, Long idUsuario){
+        return Endereco.builder()
+                .rua(dto.getRua())
+                .numero(dto.getNumero())
+                .cep(dto.getCep())
+                .complemento(dto.getComplemento())
+                .cidade(dto.getCidade())
+                .estado(dto.getEstado())
+                .usuario_id(idUsuario)
+                .build();
+    }
+
+    public Telefone paraTelefoneEntity(TelefoneDTO dto, Long idUsuario){
+        return Telefone.builder()
+                .numero(dto.getNumero())
+                .ddd(dto.getDdd())
+                .usuario_id(idUsuario)
+                .build();
+    }
+
 }
