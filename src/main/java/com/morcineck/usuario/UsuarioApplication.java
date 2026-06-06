1acbe0485a96a46a13a23da59964533cb9aa5fbd
Robson Morcineck <robsongabrielengenharia@gmail.com>
2026-06-06 18:45:38 -0300

    fix: remove pasta usuario duplicada e reorganiza estrutura

--- src/main/java/com/morcineck/usuario/UsuarioApplication.java
+++ src/main/java/com/morcineck/usuario/UsuarioApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsuarioApplication {
+public class git UsuarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioApplication.class, args);
